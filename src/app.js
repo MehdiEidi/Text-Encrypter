5bc428ba277f9b9e395d754eec474fc22ea2ae16
Mehdi Eidi <mehdiadq@gmail.com>
2020-09-03 00:50:30 +0430

    encryption and decryption functionalities added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
   
@@ -12,9 +13,40 @@ app.set('views', viewsPath)
 const port = process.env.PORT || 3000
 
 app.use(express.static(publicDirPath))
- 
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/help', (req, res) => {
+  res.send('help page')
+})
+
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {result: undefined})
+})
+
+app.post('/', (req, res) => {
+  const text = req.body.text
+  const key = Number(req.body.key)
+
+  console.log(req.body)
+
+  if(req.body.encrypt) {
+    let encryptedText = ''
+  
+    for(let i = 0; i < text.length ; i++) {
+      encryptedText = encryptedText + String.fromCharCode(text.charCodeAt(i) + key);
+    }
+  
+    res.render('index', {result: encryptedText})
+  } else {
+    let decryptedText = ''
+
+    for(let i = 0; i < text.length; i++) {
+      decryptedText = decryptedText + String.fromCharCode(text.charCodeAt(i) - key);
+    }
+
+    res.render('index', {result: decryptedText})
+  }
 })
 
 app.listen(port)
